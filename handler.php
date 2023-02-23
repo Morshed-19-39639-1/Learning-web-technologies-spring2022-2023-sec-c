3c69bed9acf44b2a00652ca838b638d850486b2c
Morshed-19-39639-1 <78311288+Morshed-19-39639-1@users.noreply.github.com>
2023-02-24 01:57:36 +0600

    complete
    complete

---
+++ handler.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Handler page</title>
+</head>
+<body>
+	<?php
+		$name = $_REQUEST['name'];
+		echo "Hello, " . $name . "!";
+        echo "<br>";
+        $email = $_REQUEST['email'];
+		echo "Hello, " . $email . "!";
+        echo "<br>";
+        $dob = $_REQUEST['dob'];
+		echo "Your date of birth is " . $dob;
+        echo "<br>";
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $gender = $_POST['gender'];
+            echo "<p>Your gender is: $gender</p>";
+          } else {
+            echo "<p>No data was submitted.</p>";
+          }
+        echo "<br>";
+        $degree = $_REQUEST['degree'];
+		echo "Your degree is " . $degree;
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $bloodGroup = $_POST['blood-group'];
+            echo "<p>Your blood group is: $bloodGroup</p>";
+          } else {
+            echo "<p>No data was submitted.</p>";
+          }
+	?>
+</body>
+</html>
