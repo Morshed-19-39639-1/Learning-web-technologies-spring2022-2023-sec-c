7efe85d2ff1a8be3e0290929fcfe1bb913a980e2
Morshed-19-39639-1 <78311288+Morshed-19-39639-1@users.noreply.github.com>
2024-09-06 02:15:50 +0600

    Update

--- lab3.1.php
+++ lab3.1.php
@@ -50,11 +50,10 @@
         <legend>
         <label for="degree">Degree:</label>
         </legend>
-        <input type="Checkbox" id="degree" name="degree" value="SSC">SSC
-        <input type="Checkbox" id="degree" name="degree" value="HSC">HSC
-        <input type="Checkbox" id="degree" name="degree" value="BSc">BSc
-        <input type="Checkbox"id="degree" name="degree" value="MSc">MSc
-		
+	        <input type="Checkbox" id="degree" name="degree" value="SSC">SSC
+	        <input type="Checkbox" id="degree" name="degree" value="HSC">HSC
+	        <input type="Checkbox" id="degree" name="degree" value="BSc">BSc
+	        <input type="Checkbox"id="degree" name="degree" value="MSc">MSc
 		<br><hr><br>
 		<input type="submit" value="Submit">
     </fieldset>
@@ -63,15 +62,15 @@
         <label for="blood-group">BLOOD GROUP :</label>
         </legend>
         <select id="blood-group" name="blood-group">
-        <option value="text"> Enter your blood group </option>
-        <option value="A+">A+</option>
-        <option value="A-">A-</option>
-        <option value="B+">B+</option>
-        <option value="B-">B-</option>
-        <option value="O+">O+</option>
-        <option value="O-">O-</option>
-        <option value="AB+">AB+</option>
-        <option value="AB-">AB-</option>
+	        <option value="text"> Enter your blood group </option>
+	        <option value="A+">A+</option>
+	        <option value="A-">A-</option>
+	        <option value="B+">B+</option>
+	        <option value="B-">B-</option>
+	        <option value="O+">O+</option>
+	        <option value="O-">O-</option>
+	        <option value="AB+">AB+</option>
+	        <option value="AB-">AB-</option>
       </select>
       <br><hr><br>
 		<input type="submit" value="Submit">
